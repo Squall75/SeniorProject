a8ff67c511a73841a0da42ef305d4c6ed252caa9
Squall75 <Illowkeyll@gmail.com>
2014-01-25 16:06:43 -0600

    Initial Commit

---
+++ DixonNav/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DixonNav
+//
+//  Created by Todd Seabrook on 1/25/14.
+//  Copyright (c) 2014 Christian Brothers University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DixonNav/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DixonNav
+//
+//  Created by Todd Seabrook on 1/25/14.
+//  Copyright (c) 2014 Christian Brothers University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
